71331092a16d0f2dbf931c5d0dd7f48e37b5af4d
Kiran Grewal <130530803+kg357@users.noreply.github.com>
2026-06-12 20:59:56 -0700

    Complete first 6 javascript exercises

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -7,8 +7,18 @@
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
+const { thisYear, birthYear } = require("./joinStrings-example")
+
 // Add your code right below, good luck!
 
+const firstName = 'Carlos'
+const lastName = 'Stevenson'
+
+const fullName = firstName + " " + lastName
+let age = thisYear - birthYear
+
+let greeting = `Hello! My name is ${fullName} and I am ${age} years old.`
+console.log(greeting)
 
 
 
--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    if(num<0) {
+        return 'ERROR';
+    } else {
+
+        // loop the word num number of times
+
+        let i = 1;
+
+        let repeatedWord = "";
+
+        while(i <= num) {
+
+            repeatedWord = repeatedWord.concat(str);
+
+            i++;
+        }
+
+        return repeatedWord;
+    }
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
