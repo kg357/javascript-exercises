610d39d07f152fbeb380ea0ca8b21ad2283bb3e0
Kiran Grewal <130530803+kg357@users.noreply.github.com>
2026-07-07 13:13:33 -0700

    Complete problem 12 and 13

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = nums.reduce(function(total, num){
+    total = num + total;
+    return total;
+  }, 0);
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let product = nums.reduce(function(product, num){
+    product = product * num;
+    return product;
+  }, 1);
 
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  for(let i = num; i > 0; i--) {
+    product = product * i;
+  }
+  return product;
 };
 
 // Do not edit below this line
