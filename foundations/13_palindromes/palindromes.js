327f7cd8adc296b502408de80902ee6c26094f92
Kiran Grewal <130530803+kg357@users.noreply.github.com>
2026-07-07 18:44:03 -0700

    Change solution to palindromes exercise 13 to exclude all symbols

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,34 @@
-const palindromes = function () {
+
+const palindromes = function (text) {
+
+    let copy = text.toLowerCase();
+    let splitCopy = copy.split("");
+
+    let filteredCopy = removePunctuationsAndSpaces(splitCopy);
+
+    let reversedList = filteredCopy.reverse();
+    let reversedText = reversedList.join("");
+    let splitText = text.toLowerCase().split("");
+
+    let filteredText = removePunctuationsAndSpaces(splitText);
+
+    let cleanText = filteredText.join("");
+    if (cleanText === reversedText) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
+function removePunctuationsAndSpaces(characters) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let filteredList = characters.filter(function (character) {
+        return alphanumerical.includes(character);
+    });
+    return filteredList;
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
