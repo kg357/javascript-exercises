196989fda707bd846aff36c1247789f01ea1cb44
Kiran Grewal <130530803+kg357@users.noreply.github.com>
2026-07-07 23:02:33 -0700

    Complete solution to fibonacci exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
 
+    num = +num;
+
+    let fib = [1, 1];
+
+    switch (num) {
+        case 0: return 0;
+        case 1: return 1;
+        case 2: return 1;
+        case 3: return 2;
+    }
+
+    if (num < 0) {
+        return "OOPS";
+    }
+
+
+    for (let i = 0; i <= num - 3; i++) {
+        fib[i + 2] = fib[i] + fib[i + 1];
+
+    }
+    return fib[num - 1];
 };
 
 // Do not edit below this line
