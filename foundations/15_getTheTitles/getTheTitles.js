b45f286b2f447aec4eed12aefef5c87bc1c97f9a
Kiran Grewal <130530803+kg357@users.noreply.github.com>
2026-07-12 19:11:45 -0700

    Complete exercise 16 findTheOldest and exercise 15 getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,20 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+    let bookTitles = books.map(book => book.title);
+    return bookTitles;
 };
+
+
+
+/*
+understand problem, plan/pseudocode, divide and con9uer, and
+test
+
+note: functions are like comments
+
+built in: reduce, sort, map, filter
+*/
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,46 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+
+    const currentYear = new Date().getFullYear();
+
+    const updated = people.map(person => ({
+        ...person,
+        age: person.yearOfDeath ?
+            person.yearOfDeath - person.yearOfBirth :
+            currentYear - person.yearOfBirth
+    }));
+
+    let oldest = updated[0];
+
+    for (let i = 0; i < updated.length; i++) {
+        if (updated[i].age > oldest.age) {
+            oldest = updated[i];
+        }
+    }
+    return oldest;
 
 };
+
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1066,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
+
+console.log(findTheOldest(people));
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
